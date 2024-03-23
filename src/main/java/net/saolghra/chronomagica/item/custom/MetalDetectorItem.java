d4dd6c6d822bce5a19918bcb296ff0f196b50beb
SaolGhra <DeathCRystalYT@Gmail.com>
2024-03-23 23:26:27 +0000

    V0.2.6 Added custom tags

--- src/main/java/net/saolghra/chronomagica/item/custom/MetalDetectorItem.java
+++ src/main/java/net/saolghra/chronomagica/item/custom/MetalDetectorItem.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.saolghra.chronomagica.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -63,6 +64,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DEEPSLATE_IRON_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
---
+++ src/main/java/net/saolghra/chronomagica/util/ModTags.java
@@ -0,0 +1,25 @@
+package net.saolghra.chronomagica.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.saolghra.chronomagica.Chronomagica;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(Chronomagica.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(Chronomagica.MOD_ID, name));
+        }
+    }
+}
